d8540e7250fd550eb3869791ff09d163fbae60ea
Matthew Rawcliffe <mar44@aber.ac.uk>
2015-03-11 21:44:45 +0000

    added some ui mockups

--- main.py
+++ main.py
@@ -9,19 +9,11 @@ import time
 
 def main():
     #u = ulam.UlamSpiral(prime.Generator, {"min": 0, "max": 62750, "width": 250, "height": 250, "colour": (0, 255, 0, 255), "bgcolour": (0, 0, 0, 255)})
-    #s = sacks.SacksSpiral(prime.Generator, {"min": 0, "max": 10000, "width": 250, "height": 250, "colour": (0, 255, 0, 255), "bgcolour": (0, 0, 0, 255)})
+    s = sacks.SacksSpiral(prime.Generator, {"min": 0, "max": 20000, "width": 250, "height": 250, "colour": (0, 255, 0, 255), "bgcolour": (0, 0, 0, 255)})
     start = time.time()
-    #s.to_image("test.png")
+    s.to_image("test.png")
     #u.to_image("test.png")
     print time.time() - start, "seconds"
-    # TODO: generators work, remove from main and write tests
-    #primes = prime.Generator(maximum=62750)
-    #primes.generate()
-    #print primes.data
-    #primes.to_file()
-    #gaussians = gaussian.Generator(minimum=complex(-100, -100), maximum=complex(100, 100))
-    #gaussians.generate()
-    #gaussians.to_file()
 
 
 if __name__ == '__main__':
