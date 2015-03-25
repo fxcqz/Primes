7738266186c9743ec9a0c6be511010f1d3eb6123
Matthew Rawcliffe <mar44@aber.ac.uk>
2015-03-25 11:47:37 +0000

    added sacks form and forms are now dynamically loaded from layout combo box

--- primes/ui/form_handler.py
+++ primes/ui/form_handler.py
@@ -1,7 +1,10 @@
 from PyQt4 import QtGui, QtCore
 
 from forms.ulam.form import Ui_FormUlam
+from forms.sacks.form import Ui_FormSacks
+
 import forms.ulam.handler as ulam_handler
+import forms.sacks.handler as sacks_handler
 
 
 class FormWrapper(QtGui.QWidget):
@@ -19,19 +22,22 @@ class FormWrapper(QtGui.QWidget):
 class FormHandler():
     def __init__(self, parent):
         self.parent = parent
-        self.form_handles = {"ulam": Ui_FormUlam()}
+        self.form_handles = {"ulam": Ui_FormUlam(),
+                             "sacks": Ui_FormSacks()}
         self.form_wrapper = None
 
     def setup_form(self, name):
-        form = FormWrapper()
-        form.set_form(name, self.form_handles[name])
-        form.setParent(self.parent)
-        self.form_wrapper = form
+        self.form_wrapper = FormWrapper()
+        self.form_wrapper.set_form(name, self.form_handles[name])
+        self.form_wrapper.setParent(self.parent)
         self.setup_connections(name)
+        self.form_wrapper.show()
 
     def setup_connections(self, name):
         if name == "ulam":
             ulam_handler.conn(self.form_wrapper.form)
+        elif name == "sacks":
+            sacks_handler.conn(self.form_wrapper.form)
 
     def retrieve_data(self):
         ret = None
@@ -39,9 +45,12 @@ class FormHandler():
             form = self.form_wrapper.form
             if self.form_wrapper.name == "ulam":
                 ret = ulam_handler.retrieve(form)
+            elif self.form_wrapper.name == "sacks":
+                ret = sacks_handler.retrieve(form)
         return ret
 
     def remove_form(self):
-        self.form_wrapper.setParent(None)
-        del self.form_wrapper
-        self.form_wrapper = None
+        if self.form_wrapper:
+            self.form_wrapper.setParent(None)
+            del self.form_wrapper
+            self.form_wrapper = None
--- primes/ui/gui.py
+++ primes/ui/gui.py
@@ -14,7 +14,20 @@ class StartGui(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.form_handler = FormHandler(self.ui.specific_settings)
+        self.setup_connections()
         self.form_handler.setup_form("ulam")
+
+    def setup_connections(self):
+        QtCore.QObject.connect(self.ui.f_layout, \
+            QtCore.SIGNAL("currentIndexChanged(QString)"), \
+            lambda: self.form_swapper(self.ui.f_layout.currentText()))
+
+    def form_swapper(self, name):
+        self.form_handler.remove_form()
+        if name == "Ulam Spiral":
+            self.form_handler.setup_form("ulam")
+        elif name == "Sacks Spiral":
+            self.form_handler.setup_form("sacks")
 
 
 def run(argv):
