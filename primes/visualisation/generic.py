512b2ae1f62d787c2f6f4aefdc95537164234a45
Matthew Rawcliffe <mar44@aber.ac.uk>
2015-04-15 19:12:47 +0100

    doc'd the two super classes for visualisations

--- primes/visualisation/generic.py
+++ primes/visualisation/generic.py
@@ -1,4 +1,21 @@
 class Generic(object):
+    """Base class for visualisations.
+
+    Sets up instance  variables which are  prevalent across all  visualisations,
+    and also  implements a stub  for `set_specifics',  which is used by some but
+    not all visualisations.
+
+    Attributes:
+        settings -- Dict holding all the settings for the visualisation.
+        generator -- handle to the generator being used by the visualisation.
+        width -- width of the visualisation.
+        height -- height of the visualisation.
+        limit -- upper limit in the dataset being used.
+
+    Arguments:
+        generator -- maps to the generator attribute.
+        settings -- maps to the settings attribute.
+    """
     def __init__(self, generator, settings):
         self.settings = settings
         self.generator = generator(self.settings["min"], self.settings["max"])
@@ -7,4 +24,12 @@ class Generic(object):
         self.limit = self.settings["max"]
 
     def set_specifics(self, data):
+        """(Stub) Some visualisations  required additional or different settings
+        to function  correctly. This function is used to set these values before
+        running the visualisation.
+
+        Arguments:
+            data -- values which will map to unique instance variables on a per-
+                    visualisation basis.
+        """
         pass
